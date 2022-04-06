46ea83a2d9743b99e23e8d4d80f7b288e4833471
karlebh <akejucaleb@gmail.com>
2022-04-06 16:46:04 +0100

    Added a remote repo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  purge: [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+  "./src/**/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
