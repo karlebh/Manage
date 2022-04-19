fa2440e747d044bd44b64d54ab297f822aab354b
karlebh <akejucaleb@gmail.com>
2022-04-19 04:04:23 +0100

    bug fixes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,9 +13,9 @@
     <!-- Moile Header -->
     <header class="lg:hidden relative">
       <svg class="absolute left-10 -top-64 md:hidden" xmlns="http://www.w3.org/2000/svg" width="814" height="814"><rect width="436" height="970" x="774" y="-62" fill="#FFF0EC" fill-rule="evenodd" rx="218" transform="rotate(45 718.814 -291.157)"/></svg>
-      <div class="flex justify-between px-8 pt-14 pb-5 relative z-50">
+      <div class="flex justify-between px-8 pt-14 pb-5">
         <Logo/>
-        <button @click="isOpen = !isOpen">
+        <button @click="isOpen = !isOpen" class="z-50">
           <svg v-if="!isOpen" xmlns="http://www.w3.org/2000/svg" width="25" height="18"><g fill="#242D52" fill-rule="evenodd"><path d="M0 0h25v4H0zM0 7h25v4H0zM0 14h25v4H0z"/></g></svg>
           <svg v-if="isOpen" xmlns="http://www.w3.org/2000/svg" width="21" height="22"><path fill="#242D52" fill-rule="evenodd" d="M17.925.747l2.828 2.828L13.328 11l7.425 7.425-2.828 2.828-7.425-7.425-7.425 7.425-2.828-2.828L7.671 11 .247 3.575 3.075.747 10.5 8.171 17.925.747z"/></svg>
         </button>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,7 @@
-<!-- -->
 <template>
-  <div class="lg:hidden fixed navbar z-40 w-full w-full h-full inset-0">
+  <div class="lg:hidden fixed navbar z-40 w-sreen h-screen inset-0">
     <button @click="$emit('closeNav')" class="fixed navbar z-40 w-full w-full h-full inset-0 cursor-pointer"></button>
-     <div class="bg-gray-100 w-10/12 py-7 mx-auto mt-40 relative z-50">
+     <div class="bg-gray-100 w-10/12 py-7 mx-auto mt-28 z-50 relative overflow-scroll" style="z-index: 100;">
       <a class="block text-center font-semibold" href="#">Pricing</a>
       <a class="block text-center mt-5 font-semibold" href="#">Products</a>
       <a class="block text-center mt-5 font-semibold" href="#">About US</a>
